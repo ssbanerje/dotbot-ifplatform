e35b5c0d7149d97ccecc8676a07895f35add725c
Subho Banerjee <ssbanerje@gmail.com>
2023-02-22 21:26:11 -0800

    Fixes nested plugin load issue

--- ifplatform.py
+++ ifplatform.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import glob
 import os
 import sys
 import dotbot
 from dotbot.dispatcher import Dispatcher
+from dotbot.util import module
+from dotbot.plugins import Clean, Create, Link, Shell
 
 
 def _inject_distro():
@@ -60,6 +63,19 @@ class IfPlatform(dotbot.Plugin):
         self._bsd = [d for d in self._distros if d.endswith('bsd')]
         self._linux = [d for d in self._distros if (d not in self._bsd) and (d != 'macos')]
 
+    def _load_plugins(self):
+        plugin_paths = self._context.options().plugins
+        plugins = []
+        for dir in self._context.options().plugin_dirs:
+            for path in glob.glob(os.path.join(dir, '*.py')):
+                plugin_paths.append(path)
+        for path in plugin_paths:
+            abspath = os.path.abspath(path)
+            plugins.extend(module.load(abspath))
+        if not self._context.options().disable_built_in_plugins:
+            plugins.extend([Clean, Create, Link, Shell])
+        return plugins
+
     def can_handle(self, directive):
         return directive in self._directives
 
@@ -83,5 +99,6 @@ class IfPlatform(dotbot.Plugin):
         dispatcher = Dispatcher(self._context.base_directory(),
                                 only=self._context.options().only,
                                 skip=self._context.options().skip,
-                                options=self._context.options())
+                                options=self._context.options(),
+                                plugins=self._load_plugins())
         return dispatcher.dispatch(data)
